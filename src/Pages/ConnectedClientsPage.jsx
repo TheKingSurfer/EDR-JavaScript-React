0435fcce86a53a265e1f3b1250d0b446e27d0b34
TheKingSurfer <130249176+TheKingSurfer@users.noreply.github.com>
2024-04-17 12:22:26 +0300

    Updated background and general style including - new button with animation, background image for both pages and more

--- src/Pages/ConnectedClientsPage.jsx
+++ src/Pages/ConnectedClientsPage.jsx
@@ -3,15 +3,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ConnectedClients from "../ConnectedClients";
 import ServerOfflineMessage from "../ServerOfflineMessage";
+import "../App.css";
 
 const ConnectedClientsPage = ({ connectedClients, isServerOnline }) => {
   return (
     <div>
-      <h1>Connected Clients Page</h1>
-      {!isServerOnline && <ServerOfflineMessage />}{" "}
-      {/* Render error message if server is offline */}
-      <ConnectedClients connectedClients={connectedClients} />
-      <Link to="/">Go back to Home Page</Link>
+      <div className="connectedClients"></div>
+      <div className="connectedClientsContent">
+        <h1>Connected Clients Page</h1>
+        {!isServerOnline && <ServerOfflineMessage />}
+        {/* Render error message if server is offline */}
+        <ConnectedClients connectedClients={connectedClients} />
+        <Link to="/">Go back to Home Page</Link>
+      </div>
     </div>
   );
 };
