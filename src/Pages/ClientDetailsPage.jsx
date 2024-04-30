2a3353c990ddba137b935bd8f8cc96d2021defa0
TheKingSurfer <130249176+TheKingSurfer@users.noreply.github.com>
2024-04-30 07:41:55 +0300

    Changed the UI - added to the other pages the design

--- src/Pages/ClientDetailsPage.jsx
+++ src/Pages/ClientDetailsPage.jsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
+import "@mui/material";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import "../App.css";
 
 const ClientDetailsPage = () => {
   const { ip, port } = useParams();
@@ -89,20 +97,54 @@ const ClientDetailsPage = () => {
 
   return (
     <div>
-      <h1>Client Details</h1>
-      {connIp && <p>Connection IP Address: {connIp}</p>}
-      {connPort && <p>Connection Port: {connPort}</p>}
-      <p>IP Address: {ip}</p>
-      <p>Port Number: {port}</p>
-      <ul>
-        {events.map((line, index) => (
-          <li key={index}>{line}</li>
+      <div className="connectedClients"></div>
+      <div className="clientDetailsContent">
+        <h1>Client Details</h1>
+        {connIp && <p>Connection IP Address: {connIp}</p>}
+        {connPort && <p>Connection Port: {connPort}</p>}
+        <p>IP Address: {ip}</p>
+        <p>Port Number: {port}</p>
+        <div>
+          <Link to="/connected-clients">Back to Connected Clients</Link>
+          <Link to={`/view-processes/${ip}/${port}`}>View Processes</Link>
+        </div>
+        {events.map((event, index) => (
+          <Card
+            key={index}
+            sx={{ minWidth: 275 }}
+            style={{
+              color: "black",
+              backgroundColor: "grey",
+              marginBottom: "20px",
+            }}
+          >
+            <CardContent>
+              <Typography
+                variant="h5"
+                gutterBottom
+                style={{ backgroundColor: "grey ", color: "black" }}
+              >
+                Event Details
+              </Typography>
+            </CardContent>
+            <ul>
+              {event.split("\n").map((line, lineIndex) => {
+                const colonIndex = line.indexOf(":");
+                if (colonIndex !== -1) {
+                  const key = line.substring(0, colonIndex).trim();
+                  const value = line.substring(colonIndex + 1).trim();
+                  return (
+                    <li key={lineIndex}>
+                      <span>{key}: </span>
+                      <span>{value}</span>
+                    </li>
+                  );
+                }
+                return null;
+              })}
+            </ul>
+          </Card>
         ))}
-      </ul>
-      <Link to="/connected-clients">Back to Connected Clients</Link>
-      <div>
-        {" "}
-        <Link to={`/view-processes/${ip}/${port}`}>View Processes</Link>
       </div>
     </div>
   );
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,8 +2,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, styled } from "@mui/material";
-import "../App.css"
-import "../Components/xbutton.css"
+import "../App.css";
+import "../Components/xbutton.css";
 
 const BlueButton = styled(Button)({
   backgroundColor: "blue",
@@ -30,10 +30,15 @@ const HomePage = () => {
     <section className="hero">
       <div className="content">
         <h1>Be More Secure.</h1>
-        <p>Monitoring Visualy the EDR activity.</p>
+        <p>Monitoring Visually the EDR activity.</p>
         <div className="box-2">
           <div className="btn btn-two">
-            <Button onClick={handleButtonClick} component={Link} to="/connected-clients" style={{ color: "white" }}>
+            <Button
+              onClick={handleButtonClick}
+              component={Link}
+              to="/connected-clients"
+              style={{ color: "white" }}
+            >
               Go to Connected Clients Page
             </Button>
           </div>
